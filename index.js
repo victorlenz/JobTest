53f9f14c28ada93716d538951a56db170eca26cc
vivek <greenfroginthepond@gmail.com>
2020-07-22 19:14:17 +0530

    setting up the routes

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const http = require('http')
 require('dotenv').config()
 
 const app = express()
+
+//create records in db
+app.post('/create',(req, res) => {
+
+})
+
+//downloads records
+app.get('/get', (req, res) => {
+    
+})
+
 app.use(express.static(__dirname + '/public'))
 
 app.listen(process.env.SRV_PORT, () => console.log(`Server Started on ${process.env.SRV_PORT}`));
